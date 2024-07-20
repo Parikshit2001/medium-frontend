4d371a53a45194a53001da385e94c87fa10e320f
Parikshit <parikshitgupta2001@gmail.com>
2024-07-21 01:49:35 +0530

    Fixed small error in publish page

--- src/pages/Publish.tsx
+++ src/pages/Publish.tsx
@@ -12,7 +12,8 @@ function Publish() {
 
   useEffect(() => {
     const token = localStorage.getItem("token")
-    if (token === undefined || "") {
+    console.log(token)
+    if (token === null || "") {
       navigate("/signin")
     }
   })
