b41d60eedf2375912dc1355e9d3929e6b699cf25
Parikshit <parikshitgupta2001@gmail.com>
2024-07-25 21:39:44 +0530

    minor

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,2 +1,2 @@
-export const URL = "http://localhost:8787"
-// export const URL = "https://backend.parikshitgupta2001.workers.dev"
+// export const URL = "http://localhost:8787"
+export const URL = "https://backend.parikshitgupta2001.workers.dev"
