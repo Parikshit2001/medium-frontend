d2ce8772008b5fba235b582d283a52766ca9543f
Parikshit <parikshitgupta2001@gmail.com>
2024-08-01 23:35:54 +0530

    Added Loading state to Blogs Page

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Appbar, BlogTile } from "../components";
+import { Appbar, BlogTile, Loading } from "../components";
 import axios from "axios";
 import { URL } from "../constants/constants";
 import { Link } from "react-router-dom";
@@ -16,12 +16,17 @@ interface getBlogsResponse {
 
 function Blogs() {
   const [blogs, setBlogs] = useState<getBlogsResponse[] | []>([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     const getBlogs = async () => {
       axios
         .get(`${URL}/api/v1/blog/bulk`, { withCredentials: true })
         .then((response) => {
           setBlogs(response.data.data);
+        })
+        .finally(() => {
+          setIsLoading(false);
         });
     };
     getBlogs();
@@ -32,6 +37,7 @@ function Blogs() {
       <Appbar />
       {/* All the blogs */}
       <div className="flex flex-col py-6 px-2 w-1/2 min-w-[480px] mx-auto">
+        {isLoading ? <Loading /> : null}
         {blogs.map((blog) => {
           const dateObject = new Date(blog.published);
           const formattedDate = `${dateObject.getFullYear()} ${dateObject.toLocaleDateString(
